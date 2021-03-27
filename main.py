8fa800048f465973e8b785715a5ec9beaa99fc21
Grsn_Chml <sysgrsn@gmail.com>
2021-03-27 17:14:44 -0600

    more code was added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import nltk
+import numpy as np
 # nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
@@ -9,13 +10,18 @@ def tokenize (sentence):
 def stem (word):
     return stemmer.stem(word.lower())
 
-def bag_of_words (tokenized_token, all_words):
+def bag_of_words (tokenized_sentence, all_words):
     '''
     sentence = ["hello", "how", "are", "you"]
     words = ["hi", "hello", "I", "you", "bye", "thank", "cool" ]
     bog = [0, 1, 0, 1, 0, 0, 0]
     '''
-    pass
+    tokenized_sentence = [stem(w) for w in tokenized_sentence]
+    bag = np.zeros(len(all_words),dtype=np.float32)
+    for idx, w in enumerate(all_words):
+        if w in tokenized_sentence:
+            bag[idx]=1.0
+    return bag
 
 
 # a = "How long does shipping take?"
@@ -27,3 +33,8 @@ def bag_of_words (tokenized_token, all_words):
 # words = ["Organize", "organizes", "organizing"]
 # stemmed_words = [stem(w) for w in words]
 # print(stemmed_words)
+
+# sentence = ["hello", "how", "are", "you"]
+# words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+# bog = bag_of_words(sentence, words)
+# print(bog)
