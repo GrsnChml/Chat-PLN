b568395b18b1fc85bdb00c188a33865a441c5710
Grsn_Chml <sysgrsn@gmail.com>
2021-03-27 18:30:41 -0600

    mejoras de errores resueltos

--- train.py
+++ train.py
@@ -58,16 +58,36 @@ batch_size = 8
 hidden_size = 8
 output_size = len(tags)
 input_size = len(X_train[0])
-
+learning_rate = 0.001
+num_epochs = 1000
 
 # print(input_size, len(all_words))
 # print(output_size, tags)
 
 dataset = ChatDataSet()
-train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=0)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = NeuralNet(input_size=input_size,hidden_size=hidden_size,num_classes=output_size).to(device)
+model = NeuralNet(input_size=input_size, hidden_size=hidden_size, num_classes=output_size).to(device)
 
 # loss and optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+for epoch in range(num_epochs):
+    for (words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(dtype=torch.long)
+
+        # forward
+        outputs = model(words)
+        loss = criterion(outputs, labels)
+
+        # backward and optimizer step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    if (epoch +1) % 100 == 0:
+        print(f'epoch {epoch+1}/{num_epochs}, loss={loss.item():.4f}')
+
+print(f'final loss, loss={loss.item():.4f}')
